5e69f387c492f2f7c40d9c521d3c6fe7fe40757c
kon <156806318+kon65138@users.noreply.github.com>
2024-04-28 09:35:01 +0100

    add js book constructr

--- js.js
+++ js.js
@@ -1,7 +1,23 @@
-const myLibrary = [];
+const myLibrary = [{
+    bookName: "Harry Potter and the Golbet of Fire",
+    author: "J.K Rowling",
+    pages: 636,
+    read: false,
+    libraryNo: 1,
+}, {
+    bookName: "The Long Earth",
+    author: "Terry Pratchett and Stephen Baxter",
+    pages: 400,
+    read: true,
+    libraryNo: 2,
+}];
 
-function Book() {
-
+function Book(name, pages, author, read, libraryNo) {
+    this.bookName = name;
+    this.pages = pages;
+    this.author = author;
+    this.read = read;
+    this.libraryNo = libraryNo;
 }
 
 function addBookToLibrary() {
